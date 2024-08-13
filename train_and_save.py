b0a9b74f1084676563cd3f2982f575cdfe8d6836
Hemantttt <94732586+Hemantttt@users.noreply.github.com>
2024-08-13 19:43:37 +0530

    Update train_and_save.py

--- train_and_save.py
+++ train_and_save.py
@@ -10,10 +10,10 @@ from sklearn.metrics import accuracy_score
 from sklearn.linear_model import LogisticRegression
 import pickle
 
-# Load and preprocess data
+
 df = pd.read_csv("Resume.csv")
 
-# Text cleaning function
+
 def clean(text):
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
     email_pattern = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b')
@@ -26,28 +26,28 @@ def clean(text):
 
 df['Resume'] = df['Resume'].apply(lambda x: clean(x))
 
-# Encode categories
+
 le = LabelEncoder()
 df['Category'] = le.fit_transform(df['Category'])
 
-# Vectorize text data
+
 tfidf = TfidfVectorizer()
 X = tfidf.fit_transform(df['Resume'])
 y = df['Category']
 
-# Split data into training and testing sets
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Train model
+
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
-# Evaluate model
+
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy of the Logistic Regression model on test data: {accuracy}")
 
-# Save the TF-IDF vectorizer and model
+
 with open('tfidf.pkl', 'wb') as file:
     pickle.dump(tfidf, file)
 
